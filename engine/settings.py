664da6a42cbf3ecdb2a29dc22e9f96d64f5dffe8
lodyne <lodgmtui@gmail.com>
2021-10-11 10:50:06 +0300

    use(env_variables): the use of environment variables to hide information
    information such as secret key,passwords,database names and so on

--- engine/settings.py
+++ engine/settings.py
@@ -11,7 +11,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+import environ
 from pathlib import Path
+
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e=w!48ju7klz=debpx#k+mth9#1+6rh)e27af@0=)vv=^y7r(x'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
